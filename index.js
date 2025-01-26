bc317ba7f662690e4d0283d42ad0adfcb71bea58
schmeb0704 <seanauditor0704@gmail.com>
2025-01-26 16:07:19 +0800

    feat: add default get message

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const mongoConnect = require('./db/connect')
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'huh' })
+})
 app.use('/words', wordRouter)
 
 app.listen(port, async () => {
