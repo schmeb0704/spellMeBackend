af7bdb1d93f48efe67ca05026da5b9e1e6a36bd1
schmeb0704 <seanauditor0704@gmail.com>
2025-01-26 17:50:12 +0800

    fix: whitelist needed sites

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const app = express()
 const port = process.env.PORT || 3000
 const wordRouter = require('./routes/words')
 const mongoConnect = require('./db/connect')
+const cors = require('cors')
+
+const allowedOrigins = [
+  'http://localhost:5173', // Frontend dev environment URL
+  'https://spellme.netlify.app/',
+]
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+}
+
+app.use(cors(corsOptions)) 
 
 app.use(express.json())
 
